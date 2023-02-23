a0f581f781d9fa83a383d9bb00ee79b66d0daa59
KuhaiAnna <kuhai.anna@gmail.com>
2023-02-23 16:10:29 +0200

    Create and render markup for task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsEl = galleryItems
+	.map(
+		({ original, preview, description }) =>
+			`<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`
+	)
+	.join('');
+
+galleryEl.innerHTML = galleryItemsEl;
+
+console.log(galleryEl);
