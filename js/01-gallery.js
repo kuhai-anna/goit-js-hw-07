4807e6c9045075edaf2501a90d0bc0d5de79fdfa
KuhaiAnna <kuhai.anna@gmail.com>
2023-02-23 20:56:14 +0200

    Add a part of the completed task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,26 +1,101 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+// Рендер розмітки
+const cardsMarkup = createCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
-const galleryEl = document.querySelector('.gallery');
+// Додавання обробника подій
+galleryContainer.addEventListener('click', onImageClick);
 
-const galleryItemsEl = galleryItems
-	.map(
-		({ original, preview, description }) =>
-			`<div class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-    </div>`
-	)
-	.join('');
+// Створення розмітки
+function createCardsMarkup(array) {
+	return array
+		.map(
+			({ original, preview, description }) =>
+				`<div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>`
+		)
+		.join('');
+}
 
-galleryEl.innerHTML = galleryItemsEl;
+// Відкриття модалки
+function onImageClick(e) {
+	e.preventDefault();
 
-console.log(galleryEl);
+	const isImageEl = e.target.classList.contains('gallery__image');
+
+	if (!isImageEl) {
+		return;
+	}
+	createModalImg(e).show();
+	addEventListener('keydown', onEscapePress);
+}
+
+// Створення модального вікна з відповідним зображенням
+function createModalImg(e) {
+	return basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`);
+}
+
+// Закриття модалки натисканням Escape
+function onEscapePress(e) {
+	if (e.code !== 'Escape') {
+		return;
+	}
+	createModalImg(e).close(() => console.log('lightbox not visible anymore'));
+	removeEventListener('keydown', onEscapePress);
+}
+
+// Реалізація лінивого завантаження
+
+// // Відкриття модалки
+// function onImageClick(e) {
+// 	e.preventDefault();
+
+// 	const isImageEl = e.target.classList.contains('gallery__image');
+
+// 	if (!isImageEl) {
+// 		return;
+// 	}
+
+// 	createModalImg(e);
+// }
+
+// let instance;
+
+// function createModalImg(e) {
+// 	instance = basicLightbox.create(
+// 		`<img src="${e.target.dataset.source}" width="800" height="600">`,
+// 		{
+// 			onShow: () => {
+// 				addEventListener('keydown', onEscapePress);
+// 			},
+
+// 			onClose: () => {
+// 				removeEventListener('keydown', onEscapePress);
+// 			},
+// 		}
+// 	);
+// 	instance.show();
+// }
+
+// // Закриття модалки натисканням Escape
+// function onEscapePress(e) {
+// 	if (e.code !== 'Escape') {
+// 		return;
+// 	}
+// 	instance.close();
+// }
+
+// // Реалізація лінивого завантаження
