cb2c4423e1367b5e6b0292f39f2e93ff6838c3d6
KuhaiAnna <kuhai.anna@gmail.com>
2023-02-23 16:03:05 +0200

    Revert "Add valid links for the first picture"
    This reverts commit 2be62028b0927bcad567766bd663fa5a7aeddba3.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,38 +2,3 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
-
-const galleryEl = document.querySelector('.gallery');
-
-const galleryItemsEl = galleryItems
-	.map(
-		({ original, preview, description }) =>
-			`<div class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-    </div>`
-	)
-	.join('');
-
-galleryEl.insertAdjacentHTML('afterbegin', galleryItemsEl);
-
-// galleryEl.innerHTML = galleryItemsEl;
-
-console.log(galleryEl);
-
-// {/* <div class="gallery__item">
-// 	<a class="gallery__link" href="large-image.jpg">
-// 		<img
-// 			class="gallery__image"
-// 			src="small-image.jpg"
-// 			data-source="large-image.jpg"
-// 			alt="Image description"
-// 		/>
-// 	</a>
-// </div>; */}
